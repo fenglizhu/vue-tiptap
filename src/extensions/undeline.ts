0c60c6627859431e8d060871e110acf9874de6ae
fenglizhu <2469069775@qq.com>
2022-07-25 16:03:36 +0800

    feat: 新增下划线class

--- src/extensions/undeline.ts
+++ src/extensions/undeline.ts
@@ -2,7 +2,7 @@ import{ Underline as TiptapUndeline }from '@tiptap/extension-underline'
 
 export class Undeline {
   constructor() {
-    TiptapUndeline.config.useCommands = () =>{
+    TiptapUndeline.config.useCommands = () => {
       return 'toggleUnderline'
     }
     return TiptapUndeline
